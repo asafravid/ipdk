6e15e47ffd521d0aa9077f9b04a085206037cd49
Asaf <46109899+asafravid@users.noreply.github.com>
2022-02-08 16:54:34 +0200

    target hugetables settings
    mount -t hugetlbfs nodev /mnt/huge
    echo 32 > /sys/devices/system/node/node0/hugepages/hugepages-524288kB/nr_hugepages

--- build/scripts/set_hugepages.sh
+++ build/scripts/set_hugepages.sh
@@ -4,34 +4,37 @@
 
 #...Setting Hugepages...#
 mkdir -p /mnt/huge
-if [ "$(mount | grep hugetlbfs)" == "" ]
-then
-	mount -t hugetlbfs nodev /mnt/huge
-fi
-if [ "$(grep huge < /etc/fstab)" == "" ]
-then
-	echo -e "nodev /mnt/huge hugetlbfs\n" >> /etc/fstab
-fi
+mount -t hugetlbfs nodev /mnt/huge
+echo 32 > /sys/devices/system/node/node0/hugepages/hugepages-524288kB/nr_hugepages
 
-if [ "$(grep nr_hugepages < /etc/sysctl.conf)" == "" ]
-then
-	echo "vm.nr_hugepages = 1024" >> /etc/sysctl.conf
+#if [ "$(mount | grep hugetlbfs)" == "" ]
+#then
+#	mount -t hugetlbfs nodev /mnt/huge
+#fi
+#if [ "$(grep huge < /etc/fstab)" == "" ]
+#then
+#	echo -e "nodev /mnt/huge hugetlbfs\n" >> /etc/fstab
+#fi
+
+#if [ "$(grep nr_hugepages < /etc/sysctl.conf)" == "" ]
+#then
+#	echo "vm.nr_hugepages = 1024" >> /etc/sysctl.conf
 	#sysctl -p /etc/sysctl.conf
-fi
+#fi
 
 #
 # Check if the kernel/mm version of hugepages exists, and set hugepages if so.
 #
-if [ -d /sys/kernel/mm/hugepages/hugepages-2048kB ] ; then
-	echo 1024 | tee /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
-fi
+#if [ -d /sys/kernel/mm/hugepages/hugepages-2048kB ] ; then
+#	echo 1024 | tee /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+#fi
 
 #
 # Check if the node version of hugepages exists, and set hugepages if so.
 #
-if [ -d /sys/devices/system/node/node0/hugepages/hugepages-2048kB ] ; then
-	echo 1024 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
-fi
-if [ -d /sys/devices/system/node/node1/hugepages/hugepages-2048kB ] ; then
-	echo 1024 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
-fi
+#if [ -d /sys/devices/system/node/node0/hugepages/hugepages-2048kB ] ; then
+#	echo 1024 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+#fi
+#if [ -d /sys/devices/system/node/node1/hugepages/hugepages-2048kB ] ; then
+#	echo 1024 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+#fi
